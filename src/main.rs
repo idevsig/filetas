23e4f08113097d889409a0aa76808c2279ef8699
Jetsung Chan <jetsungchan@gmail.com>
2025-03-11 02:35:17 +0800

    fix: 修复跳转问题

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::{env, net::SocketAddr};
 
 use axum::{
     Router,
-    body::Body,
+    body::{self, Body},
     extract::Request,
-    http::{self, HeaderMap, StatusCode, header},
-    response::{Html, IntoResponse},
+    http::{HeaderMap, HeaderValue, StatusCode, header},
+    response::{Html, IntoResponse, Response},
     routing::{any, get},
 };
 use regex::Regex;
@@ -15,131 +15,42 @@ use url::Url;
 static FILE_EXT: &str = ""; // 逗号分隔的扩展名列表
 
 // 加载错误模板并返回响应
-// fn error_response(info: &str, status: StatusCode, headers: &HeaderMap) -> Response {
-//     println!("{:?}", headers);
-//     if headers.contains_key(header::COOKIE) {
-//         let html_content = std::fs::read_to_string("templates/error.html")
-//             .unwrap_or_else(|_| info.to_string());
-//         let response_body = html_content.replace("{{ info }}", info);
-//         return (status, Html(response_body));
-//     }
-
-//     (status, Html(info.to_string()))
-// }
-
-// 代理请求处理
-async fn proxy(uri: &str, req: Request) -> impl IntoResponse {
-    // let (method, uri, headers) = (req.method().clone(), req.uri().clone(), req.headers().clone());
-
-    // let mut req_builder = client.request(method.clone(), uri.to_string());
-
-    // // 转移请求头
-    // for (key, value) in headers.iter() {
-    //     req_builder = req_builder.header(key, value);
-    // }
-
-    // // 转换请求体为数据流
-    // let data_stream: BodyDataStream = req.into_body().into_data_stream();
-
-    // // 处理数据流
-    // let req_body = reqwest::Body::wrap_stream(data_stream);
-    // req_builder = req_builder.body(req_body);
-
-    // // 发送请求并获取响应
-    // // let res = req_builder.send().await.map_err(|_| StatusCode::BAD_GATEWAY);
-
-    // let response = match req_builder.send().await {
-    //     Ok(resp) => {
-    //         // 检查响应的 Location 头以处理重定向
-    //         if let Some(location) = resp.headers().get("location") {
-    //             let new_headers = resp.headers();
-    //             if let Ok(location_str) = location.to_str() {
-    //                 if let Ok(new_uri) = location_str.parse::<Uri>() {
-    //                     let mut builder = Request::builder();
-
-    //                     for (key, value) in new_headers.iter() {
-    //                         builder = builder.header(key, value);
-    //                     }
-
-    //                     builder.method(method).
-    //                         uri(new_uri).
-    //                         body(Body::empty()).
-    //                         map_err(|_| StatusCode::INTERNAL_SERVER_ERROR);
-    //                     // 递归调用以处理重定向
-    //                     // proxy(new_uri.to_string().as_str(), req).await
-    //                 }
-    //             }
-    //         }
-
-    //         stream_response(resp).await
-    //     }
-    //     Err(_) => {
-    //         // 在这里处理错误，例如返回特定的状态码或记录日志
-    //         return Err(StatusCode::BAD_GATEWAY);
-    //     }
-    // };
-
-    // Ok(response.into_response())
-
-    // match client.request(method, uri, headers).send().await {
-    //     Ok(resp) => {
-    //         let status = resp.status();
-    //         println!("{:?}", status);
-    //         let headers = resp.headers().clone();
-
-    //         if (headers.contains_key(header::LOCATION) || headers.contains_key(header::CONTENT_TYPE)) && is_download_url(uri) {
-    //             let localtion = headers.get(header::LOCATION).unwrap().to_str().unwrap();
-    //             let mut new_headers = headers.clone();
-    //             new_headers.remove(header::LOCATION);
-    //             return proxy(localtion, req).await;
-    //         }
-    //         // let body = resp.text().await.unwrap_or_default();
-    //         // (status, headers, Html(body))
-    //         let response = stream_response(resp).await;
-    //         return response
-    //         // response.into_response()
-    //     },
-    //     Err(_) => error_response("无法访问目标地址，请检查链接是否正确", StatusCode::BAD_REQUEST, req.headers()).into_response()
-    // }
-
-    // match client.get(uri).send().await {
-    //     Ok(resp) => {
-    //         let status = resp.status();
-    //         let headers = resp.headers().clone();
-    //         let body = resp.text().await.unwrap_or_default();
-    //         (status, headers, Html(body))
-    //         // stream_response(resp).await
-    //     },
-    //     Err(_) => error_response("无法访问目标地址，请检查链接是否正确", StatusCode::BAD_REQUEST, headers)
-    // }
-    // error_response( "Invalid URL", StatusCode::BAD_REQUEST, &headers).into_response()
-    // return resp.into_response();
+fn error_response(info: &str, status: StatusCode) -> impl IntoResponse {
+    let html_content =
+        std::fs::read_to_string("templates/error.html").unwrap_or_else(|_| info.to_string());
+    let response_body = html_content.replace("{{ info }}", info);
+    (status, Html(response_body))
 }
 
-// // 流式传输响应体
-// async fn stream_response(resp: reqwest::Response) -> impl IntoResponse {
-//     let status = resp.status();
-//     let resp_headers  = resp.headers().clone();
-//     let stream = resp.bytes_stream();
-//     let body = Body::from_stream(stream);
-//     let mut response = Response::new(body);
-//     let headers = response.headers_mut();
+// 流式传输响应体
+async fn stream_response(resp: reqwest::Response) -> impl IntoResponse {
+    let status = resp.status();
+    let resp_headers = resp.headers().clone();
+    let stream = resp.bytes_stream();
+    let body = Body::from_stream(stream);
+    let mut response = Response::new(body);
+    let headers = response.headers_mut();
 
-//     // add content-type, content-length
-//     if let Some(content_type) = resp_headers.get(header::CONTENT_TYPE) {
-//         headers.insert(header::CONTENT_TYPE, content_type.clone());
-//     }
-//     if let Some(content_length) = resp_headers.get(header::CONTENT_LENGTH) {
-//         headers.insert(header::CONTENT_LENGTH, content_length.clone());
-//     }
+    // add content-type, content-length
+    if let Some(content_type) = resp_headers.get(header::CONTENT_TYPE) {
+        headers.insert(header::CONTENT_TYPE, content_type.clone());
+    }
+    if let Some(content_length) = resp_headers.get(header::CONTENT_LENGTH) {
+        headers.insert(header::CONTENT_LENGTH, content_length.clone());
+    }
 
-//     headers.insert(header::ACCESS_CONTROL_ALLOW_ORIGIN, "*".parse().unwrap());
-//     headers.insert(header::ACCESS_CONTROL_ALLOW_METHODS, "GET".parse().unwrap());
-//     headers.insert(header::ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type, Authorization".parse().unwrap());
+    headers.insert(header::ACCESS_CONTROL_EXPOSE_HEADERS, "*".parse().unwrap());
+    headers.insert(header::ACCESS_CONTROL_ALLOW_ORIGIN, "*".parse().unwrap());
+    headers.insert(header::ACCESS_CONTROL_ALLOW_HEADERS, "*".parse().unwrap());
 
-//     *response.status_mut() = status;
-//     response.into_response()
-// }
+    // delete some headers
+    headers.remove(header::SET_COOKIE);
+    headers.remove(header::CONTENT_SECURITY_POLICY);
+    headers.remove(header::CONTENT_SECURITY_POLICY_REPORT_ONLY);
+
+    *response.status_mut() = status;
+    response.into_response()
+}
 
 // 处理 / 请求
 async fn handler_index() -> Html<String> {
@@ -195,8 +106,24 @@ async fn handler_download(req: Request<Body>) -> impl IntoResponse {
     println!("");
     println!("");
 
-    // (StatusCode::OK, "OK").into_response()
-    handler(&target_url, req).await.into_response()
+    let method = reqwest::Method::from_bytes(req.method().as_str().as_bytes()).unwrap();
+    println!("{:?}", method);
+    println!("{:?}", target_url);
+    let headers = req.headers().clone();
+    // let body_bytes = axum::body::to_bytes(req.into_body(), usize::MAX).await.unwrap();
+    // let body_str = String::from_utf8(body_bytes.to_vec()).unwrap();
+    let body_str = String::new();
+    // let headers = HeaderMap::new();
+
+    if let Ok(resp) = send_request(method, &target_url, &headers, Some(body_str)).await {
+        stream_response(resp).await.into_response()
+    } else {
+        error_response(
+                "Error: Failed to send request",
+                StatusCode::INTERNAL_SERVER_ERROR,
+            )
+            .into_response()
+    }
 }
 
 // 发送请求
@@ -207,18 +134,26 @@ async fn send_request(
     body: Option<String>,
 ) -> Result<reqwest::Response, reqwest::Error> {
     let client = reqwest::Client::builder()
-        .redirect(reqwest::redirect::Policy::none())
+        // .redirect(reqwest::redirect::Policy::none())
         .build()
         .unwrap();
 
     let mut builder = client.request(method.clone(), url);
 
-    for (key, value) in headers.iter() {
-        println!("{:?}", key);
-        println!("{:?}", value);
-        println!("");
-        // builder = builder.header(key, value);
-    }
+    println!("url: {:?}", &url);
+
+    // for (key, value) in headers.iter() {
+    //     println!("key: {:?}, value: {:?}", key, value);
+    //     println!("");
+    //     if key == header::HOST {
+    //         let parsed_url = Url::parse(&url) .unwrap();
+    //         let new_value = parsed_url.host_str().unwrap().to_string();
+    //         let header_value = HeaderValue::from_str(&new_value).unwrap();
+    //         builder = builder.header(key, header_value);
+    //         continue;
+    //     }
+    //     builder = builder.header(key, value);
+    // }
 
     if let Some(body) = body.as_ref() {
         builder = builder.body(body.clone());
@@ -226,8 +161,11 @@ async fn send_request(
 
     let request = builder.build()?;
     println!("a");
+    println!("req headers: {:?}", request.headers());
     let response = client.execute(request).await?;
     println!("b");
+    let new_headers = response.headers().clone();
+    println!("new headers: {:?}", new_headers);
 
     // 检查是否是重定向响应
     if response.status().is_redirection() {
@@ -236,39 +174,13 @@ async fn send_request(
             println!("location: {:?}", location);
             if let Ok(location_str) = location.to_str() {
                 println!("Redirecting1 to: {}", location_str);
-                return Box::pin(send_request(method, &location_str, headers, body)).await;
+                return Box::pin(send_request(method, &location_str, &new_headers, body)).await;
             }
         }
     }
 
-    println!("c");
+    println!("c: {}", &url);
     Ok(response)
-}
-
-async fn middle(
-    method: &str,
-    url: &str,
-    headers: &HeaderMap,
-    body: Option<String>,
-) -> Result<reqwest::Response, reqwest::Error> {
-    let method = reqwest::Method::from_bytes(method.as_bytes()).unwrap();
-    let response = send_request(method, url, headers, body).await?;
-    println!("HELLO: {:?}", response.headers());
-    Ok(response)
-}
-
-// 主路由处理函数
-async fn handler(req_url: &str, req: Request<Body>) -> impl IntoResponse {
-    let method = req.method().as_str();
-    println!("{:?}", method);
-    println!("{:?}", req_url);
-
-    let response = middle(method, req_url, req.headers(), None).await;
-
-    println!("");
-    println!("WORLD: {:?}", response);
-
-    (StatusCode::OK, "OK")
 }
 
 // 规范化 URL（若无协议，添加 https://）
